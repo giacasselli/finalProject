bea0d1f903586454ffe470294c3160c1aca90ecb
giacasselli <101293161+giacasselli@users.noreply.github.com>
2022-03-16 14:34:27 -0400

    Update animationPlayer.java

--- src/finalproject/animationPlayer.java
+++ src/finalproject/animationPlayer.java
@@ -1,13 +1,81 @@
-/* Final Project - Animation Player
- */
-//gia
-//can u see this
 package finalproject;
-
+class Circle
+        {
+    int r;
+    int x;
+    int y;
+    String colour;
+    int border;
+    String borderColour;
+    
+    Circle(int r, int x, int y, String colour, int border, String borderColour)
+    {
+        this.r=r;
+        this.x=x;
+        this.y=y;
+        this.colour=colour;
+        this.border=border;
+        this.borderColour=borderColour;
+    }
+    
+}
+class Rectangle
+{
+    int x;
+    int y;
+    int border; 
+    String colour;
+    String borderColour;
+    int length;
+    int width;
+    
+    Rectangle(int x, int y, int border, String colour, String borderColour, int length, int width)
+    {
+        this.x=x;
+        this.y=y;
+        this.border=border;
+        this.colour=colour;
+        this.borderColour=borderColour;
+        this.length=length;
+        this.width=width;
+    }
+}
+class Line
+{
+    int startX;
+    int startY;
+    int endX;
+    int endY;
+    String colour;
+    int border;
+    
+    Line(int startX, int startY, int endX, int endY, String colour, int border)
+    {
+        this.startX=startX;
+        this.startY=startY;
+        this.endX=endX;
+        this.endY=endY;
+        this.colour=colour;
+        this.border=border;
+    }
+}
+class AnimationPlayer
+{
+    AnimationPlayer()
+    {
+        
+    }
+    void loadAnimationFromFile(String FileName)
+    {
+        
+    }
+    void run()
+    {
+        
+    }
+}
 public class animationPlayer {
     public static void main(String[] args) {
-        System.out.println("Hello World! What up!");
-        System.out.println("Can you see this output?");
-        System.out.println("hi guy");
+
     }  
 }
