bcb524873b7dc7dfea7f3c0364822ba4d8fc383b
giacasselli <101293161+giacasselli@users.noreply.github.com>
2022-03-16 14:51:20 -0400

    Update animationPlayer.java

--- src/finalproject/animationPlayer.java
+++ src/finalproject/animationPlayer.java
@@ -1,5 +1,24 @@
 package finalproject;
-class Circle
+class Shape
+{
+    void Hide(int start)
+    {   
+        
+    }
+    void Show(int start)
+    {
+        
+    }
+    void Jump(int start, int x, int y)
+    {
+        
+    }
+    void ChangeColour(int start)
+    {
+        
+    }
+}
+class Circle extends Shape
         {
     int r;
     int x;
@@ -17,9 +36,28 @@ class Circle
         this.border=border;
         this.borderColour=borderColour;
     }
-    
+    @Override
+    void Hide(int start)
+    {
+        super.Hide(start);
+    }
+    @Override
+    void Show(int start)
+    {
+        super.Show(start);
+    }
+    @Override
+    void Jump(int start, int x, int y)
+    {
+        super.Jump(start, x, y);
+    }
+    @Override
+    void ChangeColour(int start)
+    {
+        super.ChangeColour(start);
+    }
 }
-class Rectangle
+class Rectangle extends Shape
 {
     int x;
     int y;
@@ -39,8 +77,28 @@ class Rectangle
         this.length=length;
         this.width=width;
     }
+    @Override
+    void Hide(int start)
+    {
+        super.Hide(start);
+    }
+    @Override
+    void Show(int star)
+    {
+        super.Show(star);
+    }
+    @Override
+    void Jump(int start, int x, int y)
+    {
+        super.Jump(start, x, y);
+    }
+    @Override
+    void ChangeColour(int start)
+    {
+        super.ChangeColour(start);
+    }
 }
-class Line
+class Line extends Shape
 {
     int startX;
     int startY;
@@ -57,6 +115,26 @@ class Line
         this.endY=endY;
         this.colour=colour;
         this.border=border;
+    }
+    @Override
+    void Show(int start)
+    {
+        super.Show(start);
+    }
+    @Override
+    void Hide(int start)
+    {
+        super.Hide(start);
+    }
+    @Override
+    void Jump(int start, int x, int y)
+    {
+        super.Jump(start, x, y);
+    }
+    @Override
+    void ChangeColour(int start)
+    {
+        super.ChangeColour(start);
     }
 }
 class AnimationPlayer
